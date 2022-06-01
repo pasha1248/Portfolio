dc8bb43320a2f5f1a468e28cd375101498593ad4
Admin <user@Users-MacBook-Pro.local>
2022-06-01 22:22:51 +0300

    deplo3

--- src/components/Header/HeaderSocials.jsx
+++ src/components/Header/HeaderSocials.jsx
@@ -5,8 +5,8 @@ import {BsGithub} from 'react-icons/bs'
 const HeaderSocials = () => {
   return (
     <div className='header__social'>
-        <a href="https://www.linkedin.com/in/pavlo-ivashkiv-045977230/" target='_blank'><BsLinkedin/></a>
-        <a href="https://github.com/pasha1248/" target='_blank'><BsGithub/></a>
+        <a href="https://www.linkedin.com/in/pavlo-ivashkiv-045977230/" target='_blank' rel="noreferrer"><BsLinkedin/></a>
+        <a href="https://github.com/pasha1248/" target='_blank' rel="noreferrer"><BsGithub/></a>
 
     </div>
   )
